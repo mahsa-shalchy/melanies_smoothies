4d487bc676abbdf877c8338e2f8d76d8d40431a2
Mahsa Shalchy <125085211+mahsa-shalchy@users.noreply.github.com>
2025-07-25 17:59:18 +0000

    Add st.stop()

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,9 @@ st.write('the name on your smoothie will be:', name_on_order)
 
 # Get the current credentials
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
