b0a958d786b6a608595ccdf3f4fa3dafd4f77167
Mahsa Shalchy <125085211+mahsa-shalchy@users.noreply.github.com>
2025-07-25 20:13:06 +0000

    Revert changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,16 +58,13 @@ if ingredients_list:
 
     # st.write(ingredients_string)
 
-    # my_insert_stmt = f"""
-    #     INSERT INTO smoothies.public.orders (ingredients, name_on_order)
-    #     VALUES ('""" + ingredients_string +  """',
-    #     '""" + name_on_order +  """')
-    # """
-
     my_insert_stmt = f"""
-    INSERT INTO smoothies.public.orders (ingredients, name_on_order)
-    VALUES ('{ingredients_string.strip()}', '{name_on_order}')
-"""
+        INSERT INTO smoothies.public.orders (ingredients, name_on_order)
+        VALUES ('""" + ingredients_string +  """',
+        '""" + name_on_order +  """')
+    """
+
+ 
     
     # st.write( my_insert_stmt)
 
