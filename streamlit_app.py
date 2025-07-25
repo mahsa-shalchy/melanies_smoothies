e13981c530c8a99e9ca9d5b9511411eb033ef1de
Mahsa Shalchy <125085211+mahsa-shalchy@users.noreply.github.com>
2025-07-25 16:58:02 +0000

    Add API df

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ from snowflake.snowpark.functions import col
 
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width= True)
 
 # cnx = st.connection("snowflake")
 cnx = st.connection("snowflake", type="snowflake")
