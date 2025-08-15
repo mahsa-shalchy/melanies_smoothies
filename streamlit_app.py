51b99a1d68f1122322a94bc8c7d676da73be0e31
Mahsa Shalchy <125085211+mahsa-shalchy@users.noreply.github.com>
2025-08-14 20:55:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from snowflake.snowpark.functions import col
 # New section to display smoothiefroot nutrition information
 # New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
